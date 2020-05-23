a3aa9ba4a9c4834f2cf219f5d0942521274b1945
BLKirkland <16639253+BLKirkland@users.noreply.github.com>
2020-05-22 22:02:13 -0500

    updating test file for practice

--- notes/Projects/Codecademy/test.py
+++ notes/Projects/Codecademy/test.py
@@ -1,8 +1,16 @@
-def control_flow_study(motivation):
-  if motivation == True:
-    return "Lets do this!"
-  else:
-    return "Sigh...lets do this."
+def grade_converter(gpa):
+#  grade = gpa
+  if gpa >= 4.0:
+    return "A"
+  elif gpa >= 3.0 and gpa < 4:
+    return "B"
+  elif gpa >= 2.0 and gpa < 3:
+    return "C"
+  elif gpa >= 1.0 and gpa < 2:
+    return "D"
+  elif gpa >= 0.0 and gpa < 1:
+    return "F"
+  #else:
+    #return gpa
 
-print(control_flow_study(True))
-
+print(grade_converter(4.0))
